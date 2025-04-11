aa3efc5a8ea7576b4fb06b8a45de440eedf661b1
odilo <odilomurindahabi@gmail.com>
2025-04-11 13:04:51 +0200

    create isPalindrome function -clean string -cal reverse function to reverse cleanned string

--- src/strings.js
+++ src/strings.js
@@ -7,6 +7,10 @@ function reverse(string) {
   return string.split('').reverse().join('');
 }
   
+function isPalindrome(string) {
+  const cleanedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return cleanedString === reverse(cleanedString);
+}
 
 module.exports = {
   capitalize,
