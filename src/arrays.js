20fa57a8d0df92166b1dc35dc8295d83282e5b47
odilo <odilomurindahabi@gmail.com>
2025-04-11 13:52:59 +0200

    create filterEven function

--- src/arrays.js
+++ src/arrays.js
@@ -2,6 +2,11 @@ function double(arr) {
 	return arr.map((num) => num * 2);
 }
 
+function filterEven(arr) {
+	return arr.filter((num) => num % 2 !== 0);
+}
+
 module.exports = {
   double,
+  filterEven
 };
