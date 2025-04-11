eaae85c5ad5353b6fa1121238f29a56e59c2fd72
odilo <odilomurindahabi@gmail.com>
2025-04-11 12:35:41 +0200

    install jest

---
+++ src/strings.js
@@ -0,0 +1,8 @@
+function capitalize(string) {
+  if (!string) return '';
+  return string[0].upperCase() + string.slice(1);
+}
+
+function reverse(string) {
+  return string.split('').reverse().join('');
+ }
