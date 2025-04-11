582de6612313cf71735b3513a935a60c41a2179f
odilo <odilomurindahabi@gmail.com>
2025-04-11 13:43:10 +0200

    create wordFrequency function

--- src/strings.js
+++ src/strings.js
@@ -12,8 +12,32 @@ function isPalindrome(string) {
   return cleanedString === reverse(cleanedString);
 }
 
+function wordFrequency(string) {
+	const lowerCased = string.toLowerCase();
+	let cleaned = "";
+	for (let i = 0; i < lowerCased.length; i++) {
+		const char = lowerCased[i];
+		if (
+			(char >= "a" && char <= "z") ||
+			(char >= "0" && char <= "9") ||
+			char === " "
+		) {
+			cleaned += char;
+		}
+	}
+
+	const wordsArray = cleaned.split(" ").filter((word) => word !== "");
+	const frequencyMap = {};
+	for (let i = 0; i < wordsArray.length; i++) {
+		const word = wordsArray[i];
+		frequencyMap[word] = (frequencyMap[word] || 0) + 1;
+	}
+	return frequencyMap;
+}
+
 module.exports = {
-  capitalize,
-  reverse,
-  isPalindrome
+	capitalize,
+	reverse,
+	isPalindrome,
+	wordFrequency,
 };
--- tests/strings.test.js
+++ tests/strings.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverse , isPalindrome} = require('../src/strings');
+const { capitalize, reverse , isPalindrome, wordFrequency} = require('../src/strings');
 
 describe('capitalize', () => {
   test('capitalizes the first letter of the string', () => {
@@ -18,3 +18,11 @@ describe('isPalindrome', () => {
     expect(isPalindrome('amali')).toBe(false);
   })
 })
+
+describe("wordFrequency", () => {
+	test("returns a word frequency map", () => {
+		expect(wordFrequency("hello world hello")).toEqual({ hello: 2, world: 1 });
+	});
+});
+
+
