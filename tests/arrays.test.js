ce7eb4a0281ae0840664cbb32f7d303f5a5778e6
odilo <odilomurindahabi@gmail.com>
2025-04-11 13:55:36 +0200

    create test for filterEven

--- tests/arrays.test.js
+++ tests/arrays.test.js
@@ -1,7 +1,13 @@
-const { double } = require('../src/arrays');
+const { double,filterEven } = require('../src/arrays');
 
 describe("double", () => {
 	test("doubles every number in the array", () => {
 		expect(double([1, 2, 3])).toEqual([2, 4, 6]);
+	});
+});
+
+describe("filterEven", () => {
+	test("filters out even numbers", () => {
+		expect(filterEven([1, 2, 3, 4])).toEqual([1, 3]);
 	});
 });
